d939b9d69b45838b64f4978cb577afd3ea93d8b3
Marcus Rothoff <marcusrothoff@MacBook-Pro-som-tillhor-Marcus.local>
2022-11-19 18:47:34 +0100

    Add about component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {IoMdSchool} from 'react-icons/io'
+import {AiOutlineFundProjectionScreen} from 'react-icons/ai'
 
 const About = () => {
   return (
-    <section id="about">About</section>
+    <section id="about">
+      <h5>Get to know</h5>
+      <h2>About me</h2>
+      <div className='container about__container'>
+        <div className='about__me'>
+          <div className='about__me-image'>
+            <img src={ME} alt="About image" />
+          </div>
+        </div>
+        <div className='about__content'>
+          <div className='about__cards'>
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>1+ Years Working</small>
+            </article>
+            <article className='about__card'>
+              <IoMdSchool className='about__icon'/>
+              <h5>Education</h5>
+              <small>Full Stack Developer</small>
+            </article>
+            <article className='about__card'>
+              <AiOutlineFundProjectionScreen className='about__icon'/>
+              <h5>Projects</h5>
+              <small>20+</small>
+            </article>
+          </div>
+          <p>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+          </p>
+          <a href="#contact" className='btn btn-primary'>Talk?</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
