1cab8ad19cb290f696a5be3802854ce313046fa1
Marcus Rothoff <marcusrothoff@MacBook-Pro-som-tillhor-Marcus.local>
2022-11-21 18:50:10 +0100

    add testimonials component

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,70 @@
 import React from 'react'
 import './testimonials.css'
+import AVTR1 from '../../assets/avatar1.png'
+import AVTR2 from '../../assets/avatar2.png'
+import AVTR3 from '../../assets/avatar3.png'
+import AVTR4 from '../../assets/avatar4.png'
+
+// import Swiper core and required modules
+import { Pagination, A11y } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: 'Bobby Brown',
+    review: 'Honestly, he is the best i have ever encountered',
+  },
+  {
+    avatar: AVTR2,
+    name: 'Lars Mcknuckles',
+    review: 'Honestly, he is the best i have ever encountered',
+  },
+  {
+    avatar: AVTR3,
+    name: 'Prince of Persia',
+    review: 'Honestly, he is the best i have ever encountered',
+  },
+  {
+    avatar: AVTR4,
+    name: 'Fabrizio Von Va bene',
+    review: 'Honestly, he is the best i have ever encountered',
+  },
+]
 
 const Testimonials = () => {
   return (
-    <section id="testimonials">Testimonials</section>
+    <section id="testimonials">
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper className='container testimonials__container'
+      modules={[Pagination, A11y]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}>
+        {
+          data.map(({avatar, name, review}, index) => {
+            return (
+            <SwiperSlide key={index} className='testimonial'>
+              <div className='client__avatar'>
+                <img src={avatar} alt="Avatar one" />
+              </div>
+              <h5 className='client__name'>{name}</h5>
+              <small className="client__review">
+                {review}
+              </small>
+            </SwiperSlide>
+            )
+          })
+        }
+      </Swiper>
+    </section>
   )
 }
 
