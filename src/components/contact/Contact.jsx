ca23cdb4f94648e88d6951a63e063e8ab6066d90
Marcus Rothoff <marcusrothoff@MacBook-Pro-som-tillhor-Marcus.local>
2022-11-21 20:20:40 +0100

    add footer component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,60 @@
 import React from 'react'
 import './contact.css'
+import {MdEmail} from 'react-icons/md'
+import {AiFillLinkedin} from 'react-icons/ai'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_u058fvo', 'template_l69hv6r', form.current, 'jczRTBykows7XYT9O')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+      e.target.reset()
+  };
   return (
-    <section id="contact">Contact</section>
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact me</h2>
+
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          <article className='contact__option'>
+            <MdEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>myemail@gmail.com</h5>
+            <a href="mailto:myemail@gmail.com">Send a message</a>
+          </article>
+          <article className='contact__option'>
+            <AiFillLinkedin className='contact__option-icon'/>
+            <h4>Linkedin</h4>
+            <h5>Marcus Rothoff</h5>
+            <a href="https://www.linkedin.com/in/marcus-rothoff-59a079232/" target="_blank">Send a message</a>
+          </article>
+          <article className='contact__option'>
+            <AiFillLinkedin className='contact__option-icon'/>
+            <h4>Text</h4>
+            <h5>+46728552271</h5>
+            <a href="sms://+46728552271" target="_blank">Send a message</a>
+          </article>
+        </div>
+
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea name="message" rows="7" placeholder="Your Message" required></textarea>
+          <button type="submit" className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './footer.css'
+import {AiFillInstagram} from 'react-icons/ai'
+import {FaGithub} from 'react-icons/fa'
+import {BsLinkedin} from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>Marcus</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Experience</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Portfolio</a></li>
+        <li><a href="#">Testimonials</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+        <a href="https://instagram.com"><AiFillInstagram /></a>
+        <a href="https://github.com"><FaGithub /></a>
+        <a href="https://linkedin.com"><BsLinkedin /></a>
+      </div>
+
+      <div className='footer__copyright'>
+        <small>&copy; Marcus Rothoff visuals, all rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
