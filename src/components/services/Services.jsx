68302dd95ccce881620a99b397d4f13d2701626d
Marcus Rothoff <marcusrothoff@MacBook-Pro-som-tillhor-Marcus.local>
2022-11-28 14:01:28 +0100

    Remove checkmark

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './services.css'
-import {AiOutlineCheck} from 'react-icons/ai'
+// import {AiOutlineCheck} from 'react-icons/ai'
 
 const Services = () => {
   return (
@@ -34,19 +34,19 @@ const Services = () => {
             <h3>The Story</h3>
           </div>
           <ul className='service__list'>
-            <li><AiOutlineCheck className='service__list-icon'/>
+            <li>
             <p>Every website has a story - let me write it</p>
             </li>
-            <li><AiOutlineCheck className='service__list-icon'/>
+            <li>
             <p>A company has a beginning, a middle, and a future - let your visitors feel it!</p>
             </li>
-            <li><AiOutlineCheck className='service__list-icon'/>
+            <li>
             <p>As a novel, words need to hit, and as an internet website your words need SEO-value</p>
             </li>
-            <li><AiOutlineCheck className='service__list-icon'/>
+            <li>
             <p>An "about" page isn't about you, please, it's about the why.</p>
             </li>
-             <li><AiOutlineCheck className='service__list-icon'/>
+             <li>
             <p>The story - read it, believe it, wouldn't you want to visit it? <br></br><a href="https://marcroth0.github.io/p1-secret-bar/" target="_blank" rel="noreferrer">Secret bar</a></p>
             </li>
           </ul>
@@ -57,16 +57,16 @@ const Services = () => {
             <h3>Web Development</h3>
           </div>
           <ul className='service__list'>
-            <li><AiOutlineCheck className='service__list-icon'/>
+            <li>
             <p>Specalized in small- and medium company websites</p>
             </li>
-            <li><AiOutlineCheck className='service__list-icon'/>
+            <li>
             <p>Website controlled by you.</p>
             </li>
-            <li><AiOutlineCheck className='service__list-icon'/>
+            <li>
             <p>Authentication, administrator-view, write your own articles, publish your own products, have your own website.</p>
             </li>
-            <li><AiOutlineCheck className='service__list-icon'/>
+            <li>
             <p>Ecommerce - a fully formed website to take payments.</p>
             </li>
           </ul>
